dc2335383af5923a65fce675b42276362aace25d
Guilherme de Freitas Schwingel <gdschwingel@inf.ufsm.br>
2024-07-24 14:32:25 -0300

    atualização

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -7,7 +7,18 @@ export const verifyToken = (req, res, next) => {
   if (!token) return next(errorHandler(401, "Você não está autentificado!"));
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandler(403, "Seu token não é válido!"));
+    if (err) {
+      res.cookie("access_token", "", {
+        httpOnly: true,
+        expires: new Date(0),
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+        path: "/",
+      });
+      return next(errorHandler(403, "Você não está autentificado!"));
+    }
+      
+      
 
     // Se o token for válido, gere um novo token
     const newToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
